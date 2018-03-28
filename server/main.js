ec57ab1150e929d8ef4d5ff6af5a71e90a998e0a
Lucien Catonnet <lutopia@hotmail.fr>
2018-03-28 23:44:27 +0200

    Added timestamps to server logs

--- server/main.js
+++ server/main.js
@@ -8,6 +8,10 @@ var https = require('https');
 var forge = require('node-forge');
 var rsa = forge.pki.rsa;
 
+function pim_log(str) {
+    console.log((new Date()).toLocaleString()+': '+str);
+}
+
 var clients = {};
 
 var httpsServer = https.createServer({
@@ -21,7 +25,7 @@ var wssServer = new WebSocketServer({
     httpServer: httpsServer
 });
 wssServer.on('request', function(request) {
-    console.log('New connection: '+request.remoteAddress);
+    pim_log('New connection: '+request.remoteAddress);
     var connection = request.accept(null, request.origin);
     connection.sendObject = function(obj) { this.send(JSON.stringify(obj)); }
     connection.remoteAddress = request.remoteAddress;
@@ -36,17 +40,17 @@ wssServer.on('request', function(request) {
                 connection.public_pem = msg.public_pem;
                 connection.secret = '----PROOF----'+forge.util.bytesToHex(forge.random.getBytesSync(16));
                 connection.sendObject({type:'prove-auth',encrypted:public_key.encrypt(connection.secret,'RSAES-PKCS1-V1_5')});
-                console.log('Auth request: '+connection.remoteAddress);
+                pim_log('Auth request: '+connection.remoteAddress);
                 break;
             case 'auth-proof': // Client sent back proof of authentification
                 if(msg.decrypted===connection.secret) { // The proof is correct
                     clients[connection.public_pem] = connection;
                     connection.authenticated = true;
                     connection.sendObject({type:'authenticated'});
-                    console.log('Authenticated: '+connection.remoteAddress);
+                    pim_log('Authenticated: '+connection.remoteAddress);
                 } else { // The client is not who they say, close connection
                     connection.close();
-                    console.log('Unauthenticated: '+connection.remoteAddress);
+                    pim_log('Unauthenticated: '+connection.remoteAddress);
                 }
                 break;
             case 'relay-msg': // Client wants to pass message to other client
@@ -54,17 +58,17 @@ wssServer.on('request', function(request) {
                     var other_connection = clients[msg.msg.to];
                     if(other_connection) {
                         other_connection.sendObject({type:'relayed-msg',signature:msg.signature,msg:msg.msg})
-                        console.log('Passing message');
+                        pim_log('Passing message');
                     } else {
-                        console.log('Client asked for unknown other client');
+                        pim_log('Client asked for unknown other client');
                     }
                 } else {
-                    console.log('Unauthenticated client wants to pass message to other client');
+                    pim_log('Unauthenticated client wants to pass message to other client');
                 }
                 break;
         }
     });
     connection.on('close', function() {
-        console.log('Disconnected: '+connection.remoteAddress);
+        pim_log('Disconnected: '+connection.remoteAddress);
     });
 });
