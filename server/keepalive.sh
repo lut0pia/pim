73b53f710e97d098f8a769aeb16df1587105ab82
Lucien Catonnet <lutopia@hotmail.fr>
2018-03-28 22:27:38 +0200

    Added keepalive script

---
+++ server/keepalive.sh
@@ -0,0 +1,27 @@
+# Ensure this script is regularly run
+echo "* * * * * "$USER" cd "`pwd`" && "$0 > /etc/cron.d/pim-ka
+
+# Remember credentials for future automatic runs
+git config credential.helper store
+
+git fetch
+up_to_date=`git status | grep up-to-date`
+pid=`pgrep pim-server`
+
+if test -n "$pid" ; then
+	echo "Server already running"
+fi
+
+if test -z "$up_to_date" ; then
+	echo "Server out of date, updating..."
+	git pull
+fi
+
+if test -z "$pid" || test -z "$up_to_date" ; then
+	if test -n "$pid" ; then
+		echo "Killing old server process"
+		kill $pid
+	fi
+	echo "Starting new server process"
+	nodejs main.js >> log.txt&
+fi
